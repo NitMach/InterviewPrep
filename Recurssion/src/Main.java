f338df9c1700c0269b7f05289c943969a5888244
Nitesh <gaddalaynitesh08@gmail.com>
2024-10-13 12:39:41 +0530

    Created new graphs module

--- Recurssion/src/Main.java
+++ Recurssion/src/Main.java
@@ -7,5 +7,11 @@ public class Main {
         SubSets1 s = new SubSets1();
         List<List<Integer>> subset = s.subset(new int[]{1, 2,3, 4});
         System.out.println("Subsets LC(78) :- "+subset);
+
+
+        //Open close parenthesis problem
+        OpenClose o = new OpenClose();
+        List<String> result = o.generateParenthesis(3);
+        System.out.println("Open close paranthesis LC(22) :- "+result);
     }
 }
